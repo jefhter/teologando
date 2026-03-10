ceab1a36ca550abc36890435e9f1ee7a661a9c10
jefhter <jefhtercabral@gmail.com>
2026-03-10 14:21:31 -0300

    clean

--- src/js/loadModules.js
+++ src/js/loadModules.js
@@ -23,7 +23,5 @@ document.addEventListener('DOMContentLoaded', () => {
     loadComponent('header-placeholder', 'https://jefhter.github.io/teologando//src/components/header.html');
     loadComponent('footer-placeholder', 'https://jefhter.github.io/teologando//src/components/footer.html');
     loadComponent('article-placeholder', 'https://jefhter.github.io/teologando//src/components/ultimo.html');
-    // loadComponent('style-header', '');
-    // loadComponent('style-footer', '');
 });
 
